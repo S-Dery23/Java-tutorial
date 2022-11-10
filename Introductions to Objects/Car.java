4f6ed2556a662d965c55cda7254547fbb8aaeef5
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-10 16:07:27 +0000

    Created Car class

---
+++ Introductions to Objects/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    String brand;
+    double price;
+    int yearModel;
+
+    public Car(String brand, double price, int yearModel) {
+        this.brand = brand;
+        this.price = price;
+        this.yearModel = yearModel;
+    }
+}
