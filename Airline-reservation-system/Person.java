13a4a21da468924308549defafb122dfbdad5808
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-10 21:11:30 +0000

    Created Person Class

---
+++ Airline-reservation-system/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+    String name;
+    String nationality;
+    String dob;
+    String[] passport;
+    int seatNumber;
+
+    public Person(String name, String nationality, String dob, String[] passport, int seatNumber){
+        this.name = name;
+        this.nationality = nationality;
+        this.dob = dob;
+        this.passport = passport;
+        this.seatNumber = seatNumber;
+    }
+}
