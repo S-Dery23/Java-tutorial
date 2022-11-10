ff30df41d47fd7e6a8a74547ec7505408d75326a
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-10 21:50:13 +0000

    created person object in main

---
+++ Airline-reservation-system/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // String[] passport = {"Rayan Slim", "Canadian", "01//01//1111"};
+        Person seth = new Person("Rayan Slim", "Canadian", "01//01//1111", 5);
+
+        
+
+    }
+
+}
--- Airline-reservation-system/Person.java
+++ Airline-reservation-system/Person.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Person {
     String name;
     String nationality;
@@ -5,11 +7,11 @@ public class Person {
     String[] passport;
     int seatNumber;
 
-    public Person(String name, String nationality, String dob, String[] passport, int seatNumber){
+    public Person(String name, String nationality, String dob, int seatNumber){
         this.name = name;
         this.nationality = nationality;
         this.dob = dob;
-        this.passport = passport;
+        // this.passport = Arrays.copyOf(passport, passport.length);
         this.seatNumber = seatNumber;
     }
 }
