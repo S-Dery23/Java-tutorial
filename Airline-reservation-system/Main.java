b5be8770a82292c9afd27e1c2a73009048ec74f2
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-10 22:28:30 +0000

    created new person object

--- Airline-reservation-system/Main.java
+++ Airline-reservation-system/Main.java
@@ -4,13 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         // String[] passport = {"Rayan Slim", "Canadian", "01//01//1111"};
-        Person person = new Person("Rayan Slim", "Canadian", "01//01//1111", 5);
+        Person person = new Person("Rayan Slim", "Canadian", "01//01//1111", 10);
 
         System.out.println("Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n" + "Date of Birth: "
          + person.getDob() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n");
 
-         person.setSeatNumber(10);
-        System.out.println("Seat number is now " + person.getSeatNumber());
+        Person person1 = new Person(person);
+        person1.setName("Jad SLim");
+        person1.setSeatNumber(3);
+
+        System.out.println("Name: " + person1.getName() + "\n" + "Nationality: " + person1.getNationality() + "\n" + "Date of Birth: "
+         + person1.getDob() + "\n" + "Seat Number: " + person1.getSeatNumber() + "\n");
+    
     }
 
 }
