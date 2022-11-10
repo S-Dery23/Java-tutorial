5b5c5102c2692027ef46b2de167aedeaa307e3de
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-10 22:07:36 +0000

    Changed class variables to private

--- Airline-reservation-system/Main.java
+++ Airline-reservation-system/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // String[] passport = {"Rayan Slim", "Canadian", "01//01//1111"};
-        Person seth = new Person("Rayan Slim", "Canadian", "01//01//1111", 5);
+        Person person = new Person("Rayan Slim", "Canadian", "01//01//1111", 5);
 
-        
+        System.out.println("Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n" + "Date of Birth: "
+         + person.getDob() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n");
 
     }
 
--- Airline-reservation-system/Person.java
+++ Airline-reservation-system/Person.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 
 public class Person {
-    String name;
-    String nationality;
-    String dob;
-    String[] passport;
-    int seatNumber;
+    private String name;
+    private String nationality;
+    private String dob;
+    private String[] passport;
+    private int seatNumber;
 
     public Person(String name, String nationality, String dob, int seatNumber){
         this.name = name;
@@ -14,4 +14,25 @@ public class Person {
         // this.passport = Arrays.copyOf(passport, passport.length);
         this.seatNumber = seatNumber;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+    
+    public String getDob() {
+        return dob;
+    }
+
+    // public String[] getPassport(String[] passport) {
+    //     return passport;
+    // }
+
+    public int getSeatNumber(){
+        return seatNumber;
+    }
+
 }
