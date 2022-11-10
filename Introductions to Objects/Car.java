f64dd8300767ac350a4d528b82e2da98cb81d1cb
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-10 16:20:02 +0000

    Changed the variables to private and added getters

--- Introductions to Objects/Car.java
+++ Introductions to Objects/Car.java
@@ -1,11 +1,25 @@
 public class Car {
-    String brand;
-    double price;
-    int yearModel;
+    private String brand;
+    private double price;
+    private int yearModel;
 
     public Car(String brand, double price, int yearModel) {
         this.brand = brand;
         this.price = price;
         this.yearModel = yearModel;
     }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYearModel() {
+        return yearModel;
+    }
+
+
 }
