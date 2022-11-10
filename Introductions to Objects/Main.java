dae7607ec2690767da645b1e57a5e70e37c2197d
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-10 16:13:47 +0000

    Testing Car object

--- Introductions to Objects/Main.java
+++ Introductions to Objects/Main.java
@@ -3,6 +3,11 @@ public class Main{
     public static void main(String[] args) {
     
         System.out.println("hello World");
+        Car hyundai = new Car("Hyundai Accent", 70000, 2018);
+
+        System.out.println("Car Type: " + hyundai.brand);
+        System.out.println("Price: " + hyundai.price);
+        System.out.println("Year Model : " + hyundai.yearModel);
 
     }
 }
