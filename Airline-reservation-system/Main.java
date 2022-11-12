fab0375fdb6222c1efab6e041444b5fa2b62e05f
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-12 22:01:53 +0000

    updated workbook code for challenge

--- Airline-reservation-system/Main.java
+++ Airline-reservation-system/Main.java
@@ -1,23 +1,27 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        // String[] passport = {"Rayan Slim", "Canadian", "01//01//1111"};
-        Person person = new Person("Rayan Slim", "Canadian", "01//01//1111", 10);
 
-        System.out.println(person);
-        System.out.println(person.toString());
+        Person[] people = new Person[] { 
+            new Person("Cleopatra", "Egypt", "69 BC", 1),
+            new Person("Alexander the Great", "Macedon", "356 BC", 2),
+            new Person("Julius Caesar", "Rome", "100 BC", 3),
+            new Person("Hannibal", "Carthage", "247 BC", 4),
+            new Person("Confucius", "China", "551 BC", 5),
+            new Person("Pericles", "Greece", "429 BC", 6),
+            new Person("Spartacus", "Thrace", "111 BC", 7),
+            new Person("Marcus Aurelius", "Rome", "121 AD", 8),
+            new Person("Leonidas", "Greece", "540 BC", 9),
+            new Person("Sun Tzu", "China", "544 BC", 10),
+            new Person("Hammurabi", "Babylon", "1750 BC", 11),
+        };
 
-        if (person.applyPassport()) {
-            System.out.println("Congratulations " + person.getName() + " your passport has been approved");
-            person.setPassport();
-        } else {
-            System.out.println("Sorry " + person.getName() + " we were not able to process your passport");
+        Airline airline = new Airline();
+
+        for (int i = 0; i < people.length; i++) {
+
         }
 
-        System.out.println(person);
-        System.out.println(person.toString());
-    }
-
+     }
+    
 }
