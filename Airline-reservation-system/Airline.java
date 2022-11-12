16be6f1512bfd150f8d0ba7db67c42036fb9b025
Seth Sangber-Dery <sethsangberdery@gmail.com>
2022-11-12 22:59:20 +0000

    added createReservation method

--- Airline-reservation-system/Airline.java
+++ Airline-reservation-system/Airline.java
@@ -12,4 +12,12 @@ public class Airline {
     public void setPerson(Person person){
         this.people[person.getSeatNumber()-1] = new Person(person);
     }
+
+    public void createReservation(Person person) {
+        int index = person.getSeatNumber() - 1;  // seat number used as index.
+        people[index] = new Person(person);      // indexed element is set equal to the object.
+        System.out.println("Thank you " + person.getName() + " for flying with Java airlines. Your seat number is " + person.getSeatNumber() + ".\n");
+
+    }
+
 }
