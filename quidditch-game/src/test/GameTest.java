1d5f925b10bd63f468313ca1a133acdf68b86472
Seth Sangber-Dery <sethsangberdery@gmail.com>
2023-01-03 23:57:48 +0000

    tested getPlaceholder

--- quidditch-game/src/test/GameTest.java
+++ quidditch-game/src/test/GameTest.java
@@ -1,12 +1,31 @@
 package src.test;
+import org.junit.Before;
 import src.main.models.Game;
+import org.junit.Test;
+import src.main.models.Team;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class GameTest {
 
     Game game;
 
+    @Before
+    public void setup() {
+        game = new Game(
+                new Team("GRYFFINDOR", "Oliver", "Harry",
+                        new String[] {"Angelina", "Ginny", "Katie"}),
 
+                new Team("SLYTHERIN", "Vincent",  "Draco",
+                        new String[] {"Bridget", "Harper", "Malcolm"})
+        );
+    }
+
+    @Test
+    public void getPlaceholderTest(){
+        assertEquals("chaser" , game.getPlaceholder("<chaser> gets the next pass"));
+    }
 
 
 
