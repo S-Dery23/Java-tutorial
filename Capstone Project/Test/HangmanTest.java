163e3c10a8dca81f4bbce12c81c39a47410a9d6d
Seth Sangber-Dery <sethsangberdery@gmail.com>
2023-01-03 14:23:23 +0000

    updated changes

---
+++ Capstone Project/Test/HangmanTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HangmanTest {
+
+    @Test
+    public void main() {
+        String passport;
+        passport = "Set";
+        assertEquals("Set", passport);
+    }
+}
