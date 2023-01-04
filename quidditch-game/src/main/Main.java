8fc62c0f39f4ef2f5a81af88c2bb9527baaae186
Seth Sangber-Dery <sethsangberdery@gmail.com>
2023-01-04 18:08:03 +0000

    added methods

--- quidditch-game/src/main/Main.java
+++ quidditch-game/src/main/Main.java
@@ -2,29 +2,30 @@ package src.main;
 import src.main.models.Game;
 import src.main.models.Team;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     static Game game;
 
+    static final String TEAMS_FILE = "src/main/teams.txt";
+    static final String PLAYS_FILE = "src/main/plays.txt";
+
     public static void main(String[] args) {
-
-        Team home = new Team("GRYFFINDOR", "Oliver", "Harry",
-                new String[] {"Angelina", "Ginny", "Katie"});
-
-        Team away = new Team("SLYTHERIN", "Vincent",  "Draco",
-                new String[] {"Bridget", "Harper", "Malcolm"});
-
-        Game game = new Game(home, away);
-
-        Game game7 = new Game(home, away);
-
-        Game game5 = new Game(home, away);
-
-        System.out.println(game.getScore(away));
-
-        game.setScore(home, 50);
-
-        System.out.println(Game.getGameCount());
+        try{
+        String[][] data = getData();
+            game = new Game(
+                    new Team(data[0][0], data[0][1], data[0][2], new String[] {data[0][3], data[0][4], data[0][5]}),
+                    new Team(data[1][0], data[1][1], data[1][2], new String[] {data[1][3], data[1][4], data[1][5]})
+            );
+            startGame();
+            printResult();
+        }catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
 
     }
 
@@ -37,6 +38,13 @@ public class Main {
      * Inside the function:
      *   1. Returns data from TEAMS_FILE as a String[][] array
      */
+    public static String[][] getData() throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(TEAMS_FILE);
+        Scanner scanner = new Scanner(fis);
+        String[] lines = new String[] {scanner.nextLine(), scanner.nextLine()};
+        scanner.close();
+        return new String[][] { lines[0].split(","), lines[1].split(",")};
+    }
 
 
     /** Function name: startGame
@@ -46,6 +54,15 @@ public class Main {
      *    2. Prints the return from game.simulate(play)
      *        - println("\n" + <return> + "\n");
      */
+    public static void startGame() throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(PLAYS_FILE);
+        Scanner scanner = new Scanner(fis);
+        while (scanner.hasNextLine()){
+            wait(3);
+            System.out.println("\n" + game.simulate(scanner.nextLine()) + "\n");
+        }
+        scanner.close();
+    }
 
 
     /** Function name: printResult()
@@ -56,6 +73,14 @@ public class Main {
      *  
      */
 
+    public static void printResult(){
+        Team gryffindor = game.getTeam("GRYFFINDOR");
+        Team slytherin = game.getTeam("SLYTHERIN");
+        Team winner = game.getScore(gryffindor) > game.getScore(slytherin) ? gryffindor : slytherin;
+        System.out.println("\nGRYFFINDOR: " + game.getScore(gryffindor) + " SLYTHERIN: " + game.getScore(slytherin));
+        System.out.println("\n" + winner + " WINS!");
+    }
+
     /**
      * Function name: wait
      * @param sec
@@ -63,6 +88,13 @@ public class Main {
      * Inside the function:
      *  1. Make the code sleep for X seconds.
      */
+    public static void wait(int sec){
+        try {
+            TimeUnit.SECONDS.sleep(sec);
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
 
+    }
 
   }
