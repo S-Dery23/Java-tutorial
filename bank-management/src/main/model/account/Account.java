5fbeb974f6f35d313e4ec4f37771ba11616e9dfc
Seth Sangber-Dery <sethsangberdery@gmail.com>
2023-01-19 16:49:27 +0000

    created deposit() withdraw() and round() methods

--- bank-management/src/main/model/account/Account.java
+++ bank-management/src/main/model/account/Account.java
@@ -1,5 +1,7 @@
 package src.main.model.account;
 
+import java.text.DecimalFormat;
+
 public abstract class Account {
     private String id;
     private String name;
@@ -49,6 +51,14 @@ public abstract class Account {
 
     public void setBalance(double balance) {
         this.balance = balance;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract boolean withdraw(double amount);
+
+    protected double round(double amount) {
+        DecimalFormat formatter = new DecimalFormat("#.##");
+        return Double.parseDouble(formatter.format(amount));
     }
 
     public String toString() {
