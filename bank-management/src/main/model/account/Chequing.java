4dab158fe2cfbc3a348eeecc2c3bce7467d0db08
Seth Sangber-Dery <sethsangberdery@gmail.com>
2023-01-19 20:15:57 +0000

    modified withdraw() method

--- bank-management/src/main/model/account/Chequing.java
+++ bank-management/src/main/model/account/Chequing.java
@@ -6,6 +6,17 @@ public class Chequing extends Account{
         super(id, name, balance);
     }
 
+    @Override
+    public void deposit(double amount) {
+
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        super.setBalance(super.round(super.getBalance() - amount));
+        return true;
+    }
+
     public Chequing(Chequing source){
         super(source);
     }
