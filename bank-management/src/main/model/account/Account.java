6c14721446bfe2ea1bc4e28098dfe562745f9129
Seth Sangber-Dery <sethsangberdery@gmail.com>
2023-01-19 16:19:06 +0000

    Created Parent class and updated child classes

--- bank-management/src/main/model/account/Account.java
+++ bank-management/src/main/model/account/Account.java
@@ -1,6 +1,6 @@
 package src.main.model.account;
 
-public class Account {
+public abstract class Account {
     private String id;
     private String name;
     private double balance;
@@ -15,4 +15,47 @@ public class Account {
 
     }
 
+    public Account(Account source){
+        this.id = source.id;
+        this.name = source.name;
+        this.balance = source.balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException("Please input proper data");
+        }
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Please input proper data");
+        }
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String toString() {
+        return (this.getClass().getSimpleName()) +
+                "id = '" + this.id + "'" +
+                ", name = '" + this.name + "'" +
+                ", balance = " + this.balance +
+                '}';
+    }
 }
