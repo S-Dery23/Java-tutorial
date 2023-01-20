1654dba325a5f6766ac26bab2447325342a39a4f
Seth Sangber-Dery <sethsangberdery@gmail.com>
2023-01-20 22:15:40 +0000

    created withdraw and deposit tests

--- bank-management/src/test/AccountTests.java
+++ bank-management/src/test/AccountTests.java
@@ -29,4 +29,46 @@ public class AccountTests {
         accounts[0].withdraw(1440);
         assertEquals(84.51, accounts[0].getBalance());
     }
+
+    @Test
+    public void overdraft(){
+        accounts[0].withdraw(1534.43);
+        assertEquals(-15.42, accounts[0].getBalance());
+    }
+
+    @Test
+    public void overdraftLimit(){
+        accounts[0].withdraw(1726);
+        assertEquals(1524.51, accounts[0].getBalance());
+    }
+
+    @Test
+    public void withdrawalFee(){
+        accounts[1].withdraw(100);
+        assertEquals(2136.60, accounts[1].getBalance());
+    }
+
+    @Test
+    public void withdrawalInterest(){
+        accounts[2].withdraw(2434.31);
+        assertEquals(5020.31, accounts[2].getBalance());
+    }
+
+    @Test
+    public void withdrawalLimit(){
+        accounts[2].withdraw(7463.69);
+        assertEquals(2537.31, accounts[2].getBalance());
+    }
+
+    @Test
+    public void deposit(){
+        accounts[0].deposit(5000);
+        assertEquals(6524.51, accounts[0].getBalance());
+    }
+
+    @Test
+    public void loanDeposit(){
+        accounts[2].deposit(1000);
+        assertEquals(1537.31, accounts[2].getBalance());
+    }
 }
