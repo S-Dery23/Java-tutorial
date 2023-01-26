f57d3fa545a7d6f752a619675ffaddf703439217
Seth Sangber-Dery <sethsangberdery@gmail.com>
2023-01-26 02:36:02 +0000

    added fumctions

--- bank-management/src/main/Main.java
+++ bank-management/src/main/Main.java
@@ -1,11 +1,12 @@
 package src.main;
 
 import src.main.model.Bank;
-import src.main.model.Transaction;
 import src.main.model.account.Account;
-import src.main.model.account.Chequing;
-import src.main.model.account.Savings;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -15,35 +16,40 @@ public class Main {
    static String TRANSACTIONS_FILE = "src/main/data/transactions.txt";
 
     public static void main(String[] args) {
-        Account[] accounts = new Account[] {
-                new Chequing("f84c43f4-a634-4c57-a644-7602f8840870", "Michael Scott", 1524.51),
-                new Savings("ce07d7b3-9038-43db-83ae-77fd9c0450c9", "Saul Goodman", 2241.60)
-        };
-
-        for (Account account : accounts) {
-            bank.addAccount(account);
+        try{
+            ArrayList<Account> accounts = returnAccount();
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
         }
-
-        Transaction[] transactions = new Transaction[] {
-                new Transaction(Transaction.Type.WITHDRAW, 1546905600, "f84c43f4-a634-4c57-a644-7602f8840870", 624.99),
-                new Transaction(Transaction.Type.DEPOSIT, 1578700800, "f84c43f4-a634-4c57-a644-7602f8840870", 441.93),
-                new Transaction(Transaction.Type.WITHDRAW, 1547078400, "f84c43f4-a634-4c57-a644-7602f8840870", 546.72),
-                new Transaction(Transaction.Type.WITHDRAW, 1546732800, "f84c43f4-a634-4c57-a644-7602f8840870", 546.72),
-                new Transaction(Transaction.Type.DEPOSIT, 1578355200, "f84c43f4-a634-4c57-a644-7602f8840870", 635.95),
-                new Transaction(Transaction.Type.WITHDRAW, 1547078400, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 875.64),
-                new Transaction(Transaction.Type.WITHDRAW, 1578614400, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 912.45),
-                new Transaction(Transaction.Type.WITHDRAW, 1577836800, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 695.09),
-                new Transaction(Transaction.Type.WITHDRAW, 1609459200, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 917.21),
-                new Transaction(Transaction.Type.WITHDRAW, 1578096000, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 127.94),
-                new Transaction(Transaction.Type.WITHDRAW, 1546819200, "ce07d7b3-9038-43db-83ae-77fd9c0450c9", 612.52)
-        };
-
-
-        Transaction[] filteredTransactions = bank.getTransactions("f84c43f4-a634-4c57-a644-7602f8840870");
-
-        Account account = bank.getAccount("ce07d7b3-9038-43db-83ae-77fd9c0450c9");
     }
 
+    public static Account createObject(String[] values){
+        try {
+        return (Account) Class.forName("src.main.model.account" + values[0])
+                .getConstructor(String.class, String.class, double.class)
+                .newInstance(values[1], values[2], Double.parseDouble(values[3]));
+    }catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public static ArrayList<Account> returnAccount() throws FileNotFoundException{
+        FileInputStream fis = new FileInputStream(ACCOUNTS_FILE);
+        Scanner scan = new Scanner(fis);
+
+        ArrayList<Account> accounts = new ArrayList<Account>();
+        while (scan.hasNextLine()){
+            accounts.add(createObject(scan.nextLine().split(",")));
+        }
+        return accounts;
+    }
+
+    public static void loadAccount(ArrayList<Account> accounts){
+        for(Account account : accounts){
+            bank.addAccount(account);
+        }
+    }
     /**
      * Function name: wait
      * @param milliseconds
